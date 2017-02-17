44a13af922cce240b3f9289559c398f70229553e
Leonard LEUNG CHONG WO <leungc_l@etna-alternance.net>
2017-02-17 11:51:08 +0100

    more cleanup

--- app/js/index.js
+++ app/js/index.js
@@ -1,50 +1,44 @@
 /*globals $, SimpleStorage, document*/
 $(document).ready(function() {
 
-var addToLog = function(id, txt) {
-  $(id + " .logs").append("<br>" + txt);
-};
+    var addToLog = function(id, txt) {
+        $(id + " .logs").append("<br>" + txt);
+    };
 
-var addToSolarLog = function(id, txt) {
-  $(id + " .solarlogs").append("<br>" + txt);
-};
+    var addToSolarLog = function(id, txt) {
+        $(id + " .solarlogs").append("<br>" + txt);
+    };
 
+    var currentmwh = 2500;
+    var threshold = 1700;
 
-
-var currentmwh = 2500;
-var threshold = 1700;
-
-// ===========================
-// My solar energy
-// ===========================
-SolarEnergy.set(currentmwh, threshold);
-
-$("#blockchain button.setcurrentsolar").click(function() {
-    var value = parseInt($("#blockchain input.current").val(), 10);
-    currentmwh = value;
+    // ===========================
+    // My solar energy
+    // ===========================
     SolarEnergy.set(currentmwh, threshold);
-    // addToSolarLog("#blockchain", "SimpleStorage.set(" + value + ")");
-  });
 
-  $("#blockchain button.setthresholdsolar").click(function() {
-    var value = parseInt($("#blockchain input.threshold").val(), 10);
-    threshold = value;
-    SolarEnergy.set(currentmwh, threshold);
-    // addToSolarLog("#blockchain", "SimpleStorage.set(" + value + ")");
-  });
-
-  $("#blockchain button.getetherbalance").click(function() {
-    SolarEnergy.get().then(function(value) {
-      $("#blockchain .currentether").html(value.toNumber());
-    // addToSolarLog("#blockchain", "get ether balance: " + value.toNumber());
+    $("#blockchain button.setcurrentsolar").click(function() {
+        var value = parseInt($("#blockchain input.current").val(), 10);
+        currentmwh = value;
+        SolarEnergy.set(currentmwh, threshold);
     });
-  });
 
-  $("#blockchain button.sellenergy").click(function() {
-    var address = $("#blockchain input.address").val();
-    SolarEnergy.sellExcessEnergyForEther(address).then(function(result) {
-        $("#blockchain .result").html(result);
-    // addToSolarLog("#blockchain", "get ether balance: " + value.toNumber());
+    $("#blockchain button.setthresholdsolar").click(function() {
+        var value = parseInt($("#blockchain input.threshold").val(), 10);
+        threshold = value;
+        SolarEnergy.set(currentmwh, threshold);
     });
-  });
+
+    $("#blockchain button.getetherbalance").click(function() {
+        SolarEnergy.get().then(function(value) {
+            $("#blockchain .currentether").html(value.toNumber());
+        });
+    });
+
+    $("#blockchain button.sellenergy").click(function() {
+        var address = $("#blockchain input.address").val();
+        SolarEnergy.sellExcessEnergyForEther(address).then(function(result) {
+            $("#blockchain .result").html(result);
+        });
+    });
 });
